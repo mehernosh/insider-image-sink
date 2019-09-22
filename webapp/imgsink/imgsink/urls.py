e5345f4d9ba3b994587d0585d5ec644b10de35c0
m <mehernosh>
2019-09-22 22:20:57 +0530

    redirect root to gallery

--- webapp/imgsink/imgsink/urls.py
+++ webapp/imgsink/imgsink/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.urls import reverse_lazy
 
 urlpatterns = [
+    path('', 
+        RedirectView.as_view(url=reverse_lazy('gallery:index')),
+        name='index_view'
+    ),
     path('admin/', admin.site.urls),
-    path('uploader/', include('uploader.urls')),
-    path('gallery/', include('gallery.urls')),
+    path('uploader/', include(('uploader.urls', 'uploader'))),
+    path('gallery/', include(('gallery.urls', 'gallery'))),
 ]
