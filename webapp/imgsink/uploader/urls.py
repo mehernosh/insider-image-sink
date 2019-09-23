8370c4e882c304a08c8fefaf3efe5e423e34129d
m <mehernosh>
2019-09-23 15:40:33 +0530

    Add endpoint to get target image dimensions from lambda

--- webapp/imgsink/uploader/urls.py
+++ webapp/imgsink/uploader/urls.py
@@ -8,6 +8,7 @@ apiurls = [
     path('s3-upload-complete', apis.S3UploadComplete.as_view(), name='s3_upload_complete'),
     path('imgprocessor-report', apis.ImageProcessingReport.as_view(), name='imgprocessor_report'),
     path('passthrough', apis.ImageValidateAndPassThrough.as_view(), name='img_passthrough_upload'),
+    path('required-dimensions', apis.RequiredImageDimens.as_view(), name='required-dimensions'),
 ]
 
 urlpatterns = [
