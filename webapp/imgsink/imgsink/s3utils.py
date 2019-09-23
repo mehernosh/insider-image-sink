066d4cda581acb8bbffb88c55c5f1a6af65a9f56
m <mehernosh>
2019-09-23 15:25:33 +0530

    refactor for smaller methods

--- webapp/imgsink/imgsink/s3utils.py
+++ webapp/imgsink/imgsink/s3utils.py
@@ -4,7 +4,7 @@ import boto3
 from django.conf import settings
 
 def get_s3_client():
-    s3_client = boto3.client(
+    return boto3.client(
         's3', 
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID, 
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
